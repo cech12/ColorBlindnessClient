e8d082a7417eee5e3528795da8bbffb34f3cb17c
cech12 <cech12@gmail.com>
2026-04-14 13:44:35 +0200

    fixed game crash when reloading resources (F3+T)

--- forge/src/main/java/de/cech12/colorblindnessclient/client/ForgeModClientEvents.java
+++ forge/src/main/java/de/cech12/colorblindnessclient/client/ForgeModClientEvents.java
@@ -1,16 +1,26 @@
 package de.cech12.colorblindnessclient.client;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+@SuppressWarnings("unused")
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ForgeModClientEvents {
 
     @SubscribeEvent
     public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {
         ColorBlindnessKeyMappings.registerAll(event::register);
+    }
+
+    @SubscribeEvent
+    public static void addReloadListener(RegisterClientReloadListenersEvent event) {
+        if (event == null) {
+            return;
+        }
+        event.registerReloadListener(new ColorblindnessReloadListener());
     }
 
 }
